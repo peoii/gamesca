c853f292a5bda090cc30c7cbd4be232f72fdb4fd
Jamie Harrell <jharrell@gmail.com>
2017-03-23 12:23:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# games.harrell.ca
+My wife and I are gamers, both video and tabletop, so I created a quick little portal page to organize our [boardgamegeek](https://www.boardgamegeek.com/) data.
+
+If you'd like to use this script, the primary thing to change is line **3** of **index.php** to your BGG username.
+
+Otherwise, good luck, have fun, and if you have any ideas on improving it please feel free to file an [issue](https://github.com/peoii/gamesca/issues)
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+include_once "bgg.php";
+$myCollection = new BGGHandler("harrellca");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Harrell.CA - Games</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300|Montserrat:400,700' rel='stylesheet' type='text/css' async />
+    <link href='css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+    <link href='style.css' rel='stylesheet' type='text/css' />
+  </head>
+  <body>
+    <div class="sidebar">
+      <div class="rotate"><div class="inner">games.harrell<span class="postfix">.ca</span></div></div>
+      <div><i class="fa fa-code" aria-hidden="true"></i> with <i class="fa fa-beer" aria-hidden="true"></i> by <a href="//jamie.harrell.ca/">Jamie</a></div>
+    </div>
+    <div class="content">
+      <h2>Welcome to our Games Hub</h2>
+      <p>Board games are something that have challenged us, occupied our free time, and brought us closer together as a couple.</p>
+      <nav>
+        <ul>
+          <li><a href="#">Game Plays</a></li>
+          <li><a href="#">Collection</a></li>
+          <li><a href="#">Wishlist</a></li>
+        </ul>
+      </nav>
+      <div class="tab">
+        <div>
+          <h4><i class="fa fa-fw fa-lg fa-line-chart" aria-hidden="true"></i> BGG Statistics</h4>
+          <table>
+            <?php $myCollection->getUser(); ?>
+          </table>
+        </div>
+        <div>
+          <h4><i class="fa fa-fw fa-star" aria-hidden="true"></i>Highest Rated Games in our collection</h4>
+          <table class="topRated">
+            <?php $myCollection->getTopRated(10); ?>
+          </table>
+        </div>
+      </div>
+      <div class="tab">
+        <div>
+          <h4><i class="fa fa-fw fa-lg fa-gamepad" aria-hidden="true"></i> Games we've recently played</h4>
+          <ul>
+            <?php $myCollection->getRecentlyPlayed(15); ?>
+          </ul>
+        </div>
+        <div>
+          <h4><i class="fa fa-fw fa-bell" aria-hidden="true"></i>(<?php $myCollection->getShameCount(); ?>) Shame List - Games we own, but have not played</h4>
+          <ul>
+            <?php $myCollection->getShameList(); ?>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
